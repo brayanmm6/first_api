350da9dda4a12370ee7dbba1f67fa83fbbb2f9db
brayanmm6 <martinsbrayan514@gmail.com>
2024-09-24 20:14:49 -0300

    Corrigindo erro de acesso para o front

--- src/backend/server/routes/home.js
+++ src/backend/server/routes/home.js
@@ -6,6 +6,7 @@ const homeRoute = express.Router()
 homeRoute.get("/", async (req, res) => {
     try {
         const response = await db.showTasks()
+        res.header("Access-Control-Allow-Origin : http://localhost:5173")
         res.status(200).json(response)
     } catch (err) {
         res.status(500).send(err)
